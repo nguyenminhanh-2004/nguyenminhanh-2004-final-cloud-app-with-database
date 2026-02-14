dae90bef1c606cc52708b48bd00d9c712fd39bd6
nguyenminhanh-2004 <kenshibroly1@gmail.com>
2026-02-14 21:19:25 +0700

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# Yêu cầu import 7 classes:
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Tạo Inline cho Choice (Hiển thị các đáp án ngay trong trang tạo Câu hỏi)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3 # Mặc định cho 3 ô nhập đáp án
 
+# Tạo Inline cho Question
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -17,14 +23,19 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+# Đăng ký QuestionAdmin có chứa ChoiceInline
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text']
 
-# <HINT> Register Question and Choice models here
-
+# Đăng ký toàn bộ Models vào Admin site
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
